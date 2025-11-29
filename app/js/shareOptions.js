c6935065425c988cedc6f2c9a61e90b1a69bacde
Jacob Ortiz <98186544+jacoblynnortiz@users.noreply.github.com>
2025-11-29 02:32:26 -0600

    Add files via upload

--- app/js/shareOptions.js
+++ app/js/shareOptions.js
@@ -1,33 +1,20 @@
 let openShareOptionsBtn = document.getElementById('shareVideoBtn');
-let closeShareOptionsBtn = document.getElementById('closeShareOptions');
-let shareOptions = document.getElementById('shareOptions');
-
-let shareOptionCopy = document.getElementById('shareOptionCopy');
-let shareOptionFacebook = document.getElementById('shareOptionFacebook');
-let shareOptionX = document.getElementById('shareOptionX');
-
-openShareOptionsBtn.addEventListener('click', function () {
-  shareOptions.style.display = 'flex';
-});
-
-closeShareOptionsBtn.addEventListener('click', function () {
-  shareOptions.style.display = 'none';
-});
-
-// create share links
-
-// facebook link share button
-shareOptionFacebook.href = "https://www.facebook.com/sharer.php?u=" + document.URL;
-
-// X link share button
-shareOptionX.href = "https://x.com/intent/post?url=" + document.URL;
 
 // copy link share button
-shareOptionCopy.addEventListener('click', async () => {
+const shareData = {
+  title: "Revealed Radio Network",
+  text: "Check out this awesome content from Revealed Radio Network",
+  url: document.URL,
+};
+
+const resultPara = document.querySelector(".result");
+
+// Share must be triggered by "user activation"
+openShareOptionsBtn.addEventListener("click", async () => {
   try {
-    await navigator.clipboard.writeText(document.URL);
-    console.log('Text copied to clipboard!');
+    await navigator.share(shareData);
+    resultPara.textContent = "MDN shared successfully";
   } catch (err) {
-    console.error('Failed to copy: ', err);
+    resultPara.textContent = `Error: ${err}`;
   }
 });
